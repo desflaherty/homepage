990207db3010f41fc3704f5794abc80e2c9625f4
Des Flaherty <desflaherty@hotmail.com>
2019-06-10 10:54:52 +0000

    fix kindle nav

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,2 @@
- $(document).ready(function() {
-     (".nav li").on("click", function() {
-                        $(".nav li").removeClass("active");
-                        $(this).addClass("active");
-                    });
 
-                    //https://v4-alpha.getbootstrap.com/components/scrollspy/#mdo.
-                    //Scroll spy used to update navbar links on scroll so they will be on the relevant section
-
-                    $('body').scrollspy({ target: '#navbar' })
- });
  
