6c72d361e6f2fb8817d75e9de102aa00e17a07c7
Des Flaherty <desflaherty@hotmail.com>
2019-10-03 16:07:12 +0000

    added scroll back to top button to bug page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,5 +9,23 @@ $(document).ready(function () {
 
                     $('body').scrollspy({ target: '#navbar' });
                     
+                    
+                    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 150) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+
+                    
 });
  
