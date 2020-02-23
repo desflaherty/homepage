a323c180c0a412ac0489748afa8d1bedb7ffef76
Des Flaherty <desflaherty@hotmail.com>
2020-02-23 14:22:42 +0000

    fix scroll

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
                     
                     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 150) {
+        if ($(this).scrollTop() > 250) {
             $('.scrollToTop').fadeIn();
         } else {
             $('.scrollToTop').fadeOut();
